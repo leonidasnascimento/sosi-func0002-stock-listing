ec0b62827e6d7980c4252b3d03b465b1b751b810
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2019-09-02 10:40:02 -0300

    Adjustments for integration

--- sosi_func0002_stock_listing/__init__.py
+++ sosi_func0002_stock_listing/__init__.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 import azure.functions as func
 import json
+import requests
 
 from typing import List
 from configuration_manager.reader import reader
@@ -12,44 +13,58 @@ from azure.storage.blob import (
     PublicAccess
 )
 
+
+def upload_blob(blob_name: str, data: str):
+    configObj = reader('local.settings.json', 'Values')
+
+    # Saving output and logging the operation
+    # Create the BlockBlockService that is used to call the Blob service for the storage account.
+    block_blob_service = BlockBlobService(
+        account_name=configObj.get_value('AzureBlobAccountName'),
+        account_key=configObj.get_value('AzureBlobAccountKey')
+    )
+
+    # Create a container called 'quickstartblobs'.
+    container_name = configObj.get_value('AzureContainerName')
+    block_blob_service.create_container(container_name, fail_on_exist=False)
+
+    # Set the permission so the blobs are public.
+    block_blob_service.set_container_acl(
+        container_name, public_access=PublicAccess.Container)
+
+    # Uploading list
+    block_blob_service.create_blob_from_text(container_name, blob_name, data)
+    pass
+
+
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
 
     try:
-        # Loading configurations
         configObj = reader('local.settings.json', 'Values')
-        
+
         # Crawling
         crawlerObj = stock_listing_crawler()
-        returnList = crawlerObj.getStockList()
-        jsonObj = json.dumps(returnList)
+        stockList = crawlerObj.getStockList()
 
-        # Saving output and logging the operation
-        # Create the BlockBlockService that is used to call the Blob service for the storage account.
-        block_blob_service = BlockBlobService(
-            account_name=configObj.get_value('AzureBlobAccountName'), 
-            account_key=configObj.get_value('AzureBlobAccountKey')
-        )
+        for s in stockList:
+            jsonAux = json.dumps(stockList.get(s))
 
-        # Create a container called 'quickstartblobs'.
-        container_name = configObj.get_value('AzureContainerName')
-        blob_name = configObj.get_value('AzureBlobName')
-        block_blob_service.create_container(container_name, fail_on_exist=False)
+            headers = {
+                'content-type': "application/json",
+                'cache-control': "no-cache",
+                'postman-token': "652ec406-7b16-40ca-8436-5baf1d36b793"
+            }
 
-        # Set the permission so the blobs are public.
-        block_blob_service.set_container_acl(
-            container_name, public_access=PublicAccess.Container)
+            response: requests.Response = requests.request("POST", configObj.get_value(
+                "StockListingServiceEndPoint"), data=jsonAux, headers=headers)
 
-        # Uploading list
-        block_blob_service.create_blob_from_text(container_name, blob_name, str(jsonObj))
-
-        # Commiting to a database
-
-        # if mytimer.past_due:
-        #     logging.info('The timer is past due!')
-
-        # logging.info('Python timer trigger function ran at %s', utc_timestamp)
-
+            blob_data = ('STATUS => ' + str(response.status_code) +
+                         '\nREASON => ' + str(response.reason) +
+                         '\nMESSAGE => ' + str(response.text) +
+                         '\n\n' + jsonAux)
+            upload_blob(s, blob_data)
         pass
     except Exception as ex:
         error_log = '{} -> {}'
