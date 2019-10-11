1fc8d6cf2ca712d3b46ac37dd556296dce77dc29
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2019-10-11 09:42:10 -0300

    Implementation of US #43 & #46 of product's implementation backlog

--- sosi_func0002_stock_listing/__init__.py
+++ sosi_func0002_stock_listing/__init__.py
@@ -17,17 +17,17 @@ from azure.storage.blob import (
 SETTINGS_FILE_PATH = pathlib.Path(__file__).parent.parent.__str__() + "//local.settings.json"
 
 def upload_blob(blob_name: str, data: str):
-    configObj = reader(SETTINGS_FILE_PATH, 'Values')
+    config_obj = reader(SETTINGS_FILE_PATH, 'Values')
 
     # Saving output and logging the operation
     # Create the BlockBlockService that is used to call the Blob service for the storage account.
     block_blob_service = BlockBlobService(
-        account_name=configObj.get_value('AzureBlobAccountName'),
-        account_key=configObj.get_value('AzureBlobAccountKey')
+        account_name=config_obj.get_value('AzureBlobAccountName'),
+        account_key=config_obj.get_value('AzureBlobAccountKey')
     )
 
     # Create a container called 'quickstartblobs'.
-    container_name = configObj.get_value('AzureContainerName')
+    container_name = config_obj.get_value('AzureContainerName')
     block_blob_service.create_container(container_name, fail_on_exist=False)
 
     # Set the permission so the blobs are public.
@@ -38,20 +38,19 @@ def upload_blob(blob_name: str, data: str):
     block_blob_service.create_blob_from_text(container_name, blob_name, data)
     pass
 
-
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
     try:
-        configObj = reader(SETTINGS_FILE_PATH, 'Values')
+        config_obj = reader(SETTINGS_FILE_PATH, 'Values')
 
         # Crawling
-        crawlerObj = stock_listing_crawler()
-        stockList = crawlerObj.getStockList()
+        crawler_obj = stock_listing_crawler()
+        stock_list = crawler_obj.get_stock_list()
 
-        for s in stockList:
-            jsonAux = json.dumps(stockList.get(s))
+        for s in stock_list:
+            jsonAux = json.dumps(stock_list.get(s))
 
             headers = {
                 'content-type': "application/json",
@@ -59,7 +58,7 @@ def main(mytimer: func.TimerRequest) -> None:
                 'postman-token': "652ec406-7b16-40ca-8436-5baf1d36b793"
             }
 
-            response: requests.Response = requests.request("POST", configObj.get_value(
+            response: requests.Response = requests.request("POST", config_obj.get_value(
                 "StockListingServiceEndPoint"), data=jsonAux, headers=headers)
 
             blob_data = ('STATUS => ' + str(response.status_code) +
--- sosi_func0002_stock_listing/crawler.py
+++ sosi_func0002_stock_listing/crawler.py
@@ -3,20 +3,20 @@ import datetime
 
 from typing import List
 from .models.stock import stock
-from bs4 import BeautifulSoup
+from bs4 import (BeautifulSoup, ResultSet)
 
 class stock_listing_crawler():
     companies_index = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    site_url = 'https://br.advfn.com/bolsa-de-valores/bovespa/{}'
+    stock_list_url = 'https://br.advfn.com/bolsa-de-valores/bovespa/{}'
 
     def __init__(self):
         pass
     
-    def getStockList(self) -> dict:
+    def get_stock_list(self) -> dict:
         returnDict = dict()
 
         for index in self.companies_index:
-            url = self.site_url.format(index)
+            url = self.stock_list_url.format(index)
             req = urllib3.PoolManager()
             res = req.request('GET', url)
             soup = BeautifulSoup(res.data, 'html.parser')
@@ -39,16 +39,103 @@ class stock_listing_crawler():
 
                 if not stockRow: 
                     continue
+                
+                stock_obj = stock()
+                stock_obj.code = stockRow[1].text 
+                stock_obj.date_time_operation = datetime.datetime.utcnow().replace(
+                    tzinfo=datetime.timezone.utc).isoformat()
 
-                utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
-                stock_code = stockRow[1].text
-                company = stockRow[0].text
-                details = ''
+                if not stock_code_details_crawler().enrich(stock_obj):
+                    continue
 
-                stockObj = stock(stock_code, company, details, utc_timestamp)
+                if not stock_available_volume_cvm_code_crawler().enrich(stock_obj):
+                    continue
 
-                returnDict[stockObj.code] = stockObj.__dict__
+                returnDict[stock_obj.code] = stock_obj.__dict__
             pass
 
         return returnDict
+    pass
+
+class stock_code_details_crawler():
+    stock_det_url = "https://br.advfn.com/bolsa-de-valores/bovespa/{}/cotacao"
+      
+    def __init__(self):
+        pass
+
+    def enrich(self, stock_ref: stock) -> bool:
+        url_det = str(self.stock_det_url).format(stock_ref.code)
+        req = urllib3.PoolManager()
+        res = req.request('GET', url_det)
+        soup = BeautifulSoup(res.data, 'html.parser')
+        divs_details = soup.find_all('div', {'class': 'TableElement'})
+
+        if (not (divs_details is None)) and len(divs_details) > 0:
+            div_1_det = divs_details[0].find('table').findAll('td')
+            div_2_det = divs_details[2].find('table').findAll('td')
+            
+            stock_ref.detail = div_1_det[len(div_1_det) - 1].text
+            stock_ref.stock_name = div_1_det[0].text
+            stock_ref.isin_code = div_1_det[4].text
+            stock_ref.stock_type = self.__set_stock_type(div_1_det[3].text)
+            stock_ref.currency = div_2_det[len(div_2_det) - 1].text
+
+            return True
+
+        return False
+    
+    def __set_stock_type(self, stock_type: str):
+        if stock_type.lower() == 'preferencial':
+            return 'PN'
+        else:
+            if stock_type.lower() == 'ordinária':
+                return 'ON'
+            else:
+                return stock_type
+        pass
+    pass
+
+class stock_available_volume_cvm_code_crawler():
+    url = "https://br.advfn.com/bolsa-de-valores/bovespa/{}/empresa"
+    
+    def __init__(self):
+        pass
+
+    def enrich(self, stock_ref: stock) -> bool:
+        url_det = str(self.url).format(stock_ref.code)
+        req = urllib3.PoolManager()
+        res = req.request('GET', url_det)
+        soup = BeautifulSoup(res.data, 'html.parser')
+        res_url = res.geturl()
+
+        if str(res_url).__contains__('cotacao'):
+            return False
+
+        # CVM code
+        cvm_row = soup.find("td", text=" Código CVM ")
+
+        if cvm_row is not None:
+            cvm_code_aux = cvm_row.find_next("td").text
+            stock_ref.cvm_code = str(cvm_code_aux).strip()
+        else:
+            return False
+
+        # Stock volume
+        
+        on_share_row = soup.find("td", text="Ações Ordinárias")
+        pn_share_row = soup.find("td", text="Ações Preferenciais")
+
+        if on_share_row is not None and pn_share_row is not None:
+            on_share = on_share_row.find_next("td").text.strip().replace('.', '')
+            pn_share = pn_share_row.find_next("td").text.strip().replace('.', '')
+
+            if(stock_ref.stock_type.upper() == 'PN'):
+                stock_ref.available_volume = int(pn_share)
+            else:
+                stock_ref.available_volume = int(on_share)
+            pass
+        else:
+            return False
+        
+        return True
     pass
